ea2e80d683da59e3de9705ebfec8a0cc91752930
LucaCatalano23 <lucacatalanolc2001@gmail.com>
2023-07-10 16:38:23 +0200

    Eliminazione delle funzioni non utili all'utilizzo di una struttura dati

--- ElemType.c
+++ ElemType.c
@@ -7,38 +7,10 @@
 //Inlcude di elemtype.h
 #include "ElemType.h"
 
-#if INT_TYPE
 //return a value > 0 if el1 is greater than el2, value = 0 if inputs are equal, value < 0 if el1 < el2
-int elemCompare(const ElemType_t *el1, const ElemType_t *el2) {
+/*int elemCompare(const ElemType_t *el1, const ElemType_t *el2) {
 	return (*el1 > *el2) - (*el1 < *el2);
-}
-
-
-//returns a copy of the input elements
-void elemCopy(const ElemType_t *src, ElemType_t *dest) {
-	if(src == NULL || dest == NULL) return;
-	dest = &src;
-}
-
-
-//swaps the elements content
-void elemSwap(ElemType_t *el1, ElemType_t *el2) {
-	ElemType_t tmp = *el1;
-	*el1 = *el2;
-	*el2 = tmp;
-}
-
-//free the memory allocated for the element
-void elemDel(ElemType_t *el) {
-	(void)el;
-}
-
-//print element on file
-void elemWrite(const ElemType_t *el, FILE *f) {
-	fprintf(f, "%d", *el);
-}
-
-#else /* INT_TYPE */
+}*/
 
 //return a value > 0 if el1 age is greater than el2 age, value = 0 if equal, value < 0 otherwise
 int elemCompare(const ElemType_t *el1, const ElemType_t *el2) {
@@ -73,4 +45,3 @@ void elemWrite(const ElemType_t *el, FILE *f) {
 	fprintf(f, "%s is %d years old and is %.2fm tall and weigths %.1fKg", el->name, el->age, el->height, el->weight);
 }
 
-#endif /* INT_TYPE */
