73003d2de8417fe7fe74bfde38a176ccf99db73a
LucaCatalano23 <lucacatalanolc2001@gmail.com>
2023-07-10 16:41:23 +0200

    Eliminazione della possibilità di inserire nel buffer un singolo dato intero

--- ElemType.h
+++ ElemType.h
@@ -11,14 +11,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define INT_TYPE 0
-
-#if INT_TYPE
-
-typedef int ElemType_t;
-
-#else
-
 struct UserDataStruct_t {
 	uint8_t age;
 	char name[16];
@@ -27,8 +19,6 @@ struct UserDataStruct_t {
 };
 
 typedef struct UserDataStruct_t ElemType_t;
-
-#endif /* INT_TYPE */
 
 //return a value > 0 if el1 is greater than el2, value = 0 if inputs are equal, value < 0 if el1 < el2
 int elemCompare(const ElemType_t *el1, const ElemType_t *el2);
